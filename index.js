e7c2e1d8bf72c67833821ce211fcb73a76c6e765
T0KK3N <daniel.mora.ortiz@udla.edu.ec>
2021-06-08 20:51:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import './style.css';
+
+$(document).ready(function() {
+  $('#button').click(function() {
+    var toAdd = $('input[name=itemCompras]').val();
+    $('ol').append('<li>' + toAdd + '</li>');
+    $('input').val('');
+  });
+
+  $('input[name=itemCompras]').keyup(function(event) {
+    if (event.keyCode == 13) {
+      $('#button').click();
+    }
+  });
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+  });
+
+  $(document).on('dblclick', 'li', function() {
+    $(this)
+      .toggleClass('strike')
+      .fadeOut(500, function() {
+        $(this).remove();
+      });
+  });
+});
